5db18d1df114d2d1b7cec64cffb026d91e01dc74
Ben Kutil <ben@make-things.com>
2015-12-29 23:31:30 -0500

    :zap: add wordpress skeleton - utilizes similar setup from https://github.com/markjaquith/WordPress-Skeleton

---
+++ example-project/public/www/local-config.php
@@ -0,0 +1,12 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+
+define( 'DB_NAME', 'example' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'password' );
+define( 'DB_HOST', 'mysql' ); // Probably 'localhost'
